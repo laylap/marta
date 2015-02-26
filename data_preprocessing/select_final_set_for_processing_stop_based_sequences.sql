0098d0f5b7944ab6985e4e0867ce85acb7a88299
laylap <pournajaf@gmail.com>
2015-02-26 18:57:29 -0500

    modified

--- data_preprocessing/select_final_set_for_processing_stop_based_sequences.sql
+++ data_preprocessing/select_final_set_for_processing_stop_based_sequences.sql
@@ -1,5 +1,17 @@
-﻿SELECT distinct g.msgdate, g.route, g.trip_id, g.direction as direction_name, t.direction_id, g.vehicle, g.stop_sequence::int as stopseq, round(avg(g.adherence)) as adherence, g.arrival_time, g.departure_time, g.stop_id, g.stop_lat as latitude, g.stop_lon as longitude
+﻿drop table if exists public.gps_april_may_updated_distinct;
+SELECT g.msgdate, g.route, g.trip_id, t.direction_id, g.stop_sequence::int as stopseq, round(avg(g.adherence)) as adherence, g.arrival_time, g.departure_time, g.stop_id, g.stop_lat as latitude, g.stop_lon as longitude
 into public.gps_april_may_updated_distinct
 FROM public.gps_april_may_updated g inner join gtfs_april_may.trips t on t.trip_id = g.trip_id inner join gtfs_april_may.directions d on d.direction_name = g.direction and d.direction_id::int = t.direction_id::int 
-group by g.msgdate, g.route, g.trip_id, g.direction, t.direction_id, g.vehicle, stopseq, g.arrival_time, g.departure_time, g.stop_id,g.stop_lat,g.stop_lon
-order by msgdate,route, trip_id, direction_name, vehicle, stopseq;
+inner join gtfs_april_may.routes r on r.route_short_name = g.route and t.route_id = r.route_id
+group by g.msgdate, g.route, g.trip_id, t.direction_id, stopseq, g.arrival_time, g.departure_time, g.stop_id,g.stop_lat,g.stop_lon;
+
+/*select count(*) from public.gps_april_may_updated_distinct
+union all
+select count(*) from 
+(select distinct trip_id, msgdate, stopseq from public.gps_april_may_updated_distinct) as foo*/
+
+/*select count(*),trip_id,msgdate,stopseq from public.gps_april_may_updated_distinct
+group by trip_id,msgdate,stopseq
+order by count desc
+limit 10*/
+
