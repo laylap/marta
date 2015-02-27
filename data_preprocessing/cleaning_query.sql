06072060ea0e32afb96ccbfb5963dae8a308517b
laylap <pournajaf@gmail.com>
2015-02-26 19:00:17 -0500

    renamed

--- data_preprocessing/cleaning_query.sql
+++
@@ -1,4 +0,0 @@
-﻿drop table marta_gps_clean;
-SELECT distinct row_id, adherence::int, blockid, block_abbr, direction, lattitude::real, 
-       longitude::real, date(msgtime) as msgdate, pg_catalog.time(msgtime) as msgtime, route, stopid, timepoint, tripid, vehicle
-  FROM marta_gps where adherence <>'' and tripid::int <> 0
