ce0ff7f76964fe9934170d383d1055d044529a93
laylap <pournajaf@gmail.com>
2015-02-26 19:30:13 -0500

    newfiles

--- data_preprocessing/select_final_set_for_processing_stop_based_sequences.sql
+++ data_preprocessing/select_final_set_for_processing_stop_based_sequences.sql
@@ -1,16 +1,16 @@
-﻿drop table if exists public.gps_april_may_updated_distinct;
+﻿drop table if exists public.gps_june_aug_updated_distinct;
 SELECT g.msgdate, g.route, g.trip_id, t.direction_id, g.stop_sequence::int as stopseq, round(avg(g.adherence)) as adherence, g.arrival_time, g.departure_time, g.stop_id, g.stop_lat as latitude, g.stop_lon as longitude
-into public.gps_april_may_updated_distinct
-FROM public.gps_april_may_updated g inner join gtfs_april_may.trips t on t.trip_id = g.trip_id inner join gtfs_april_may.directions d on d.direction_name = g.direction and d.direction_id::int = t.direction_id::int 
-inner join gtfs_april_may.routes r on r.route_short_name = g.route and t.route_id = r.route_id
+into public.gps_june_aug_updated_distinct
+FROM public.gps_june_aug_updated g inner join gtfs_june_aug.trips t on t.trip_id = g.trip_id inner join gtfs_june_aug.directions d on d.direction_name = g.direction and d.direction_id::int = t.direction_id::int 
+inner join gtfs_june_aug.routes r on r.route_short_name = g.route and t.route_id = r.route_id
 group by g.msgdate, g.route, g.trip_id, t.direction_id, stopseq, g.arrival_time, g.departure_time, g.stop_id,g.stop_lat,g.stop_lon;
 
-/*select count(*) from public.gps_april_may_updated_distinct
+/*select count(*) from public.gps_june_aug_updated_distinct
 union all
 select count(*) from 
-(select distinct trip_id, msgdate, stopseq from public.gps_april_may_updated_distinct) as foo*/
+(select distinct trip_id, msgdate, stopseq from public.gps_june_aug_updated_distinct) as foo*/
 
-/*select count(*),trip_id,msgdate,stopseq from public.gps_april_may_updated_distinct
+/*select count(*),trip_id,msgdate,stopseq from public.gps_june_aug_updated_distinct
 group by trip_id,msgdate,stopseq
 order by count desc
 limit 10*/
---
+++ data_upload_scripts/create_insert_gtfs_directions.sql
@@ -0,0 +1,14 @@
+﻿DROP TABLE if exists gtfs_june_aug.directions;
+
+CREATE TABLE gtfs_june_aug.directions
+(
+  direction_id integer,
+  direction_name character varying
+);
+
+insert into gtfs_june_aug.directions (direction_id,direction_name)
+values
+(0,'Northbound'),
+(1,'Southbound'),
+(0,'Eastbound'),
+(1,'Westbound');
