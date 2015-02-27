c0b5e1df611ab4d9e4fca355262dc537b9dac38c
laylap <pournajaf@gmail.com>
2015-02-26 19:30:53 -0500

    modified

--- data_preprocessing/find_stop_ids.sql
+++ data_preprocessing/find_stop_ids.sql
@@ -8,5 +8,5 @@ group by row_id) as Y inner join viewX Z on Y.row_id=Z.row_id and Y.mindist=Z.di
 CREATE VIEW viewX as
 select A.*, st.stop_lat, st.stop_lon, ST_DISTANCE(ST_MAKEPOINT(A.longitude::float,A.lattitude::float), ST_MAKEPOINT(st.stop_lon::float,st.stop_lat::float)) as distance from 
 (select * from gps_april_may as g inner join gtfs_april_may.stop_times s on g.tripid=s.trip_id ) 
-as A inner join gtfs_april_may.stops st on A.stop_id=st.stop_id; 
-*/
+as A 
+inner join gtfs_april_may.stops st on A.stop_id=st.stop_id; */
